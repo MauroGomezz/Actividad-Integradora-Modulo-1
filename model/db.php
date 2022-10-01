358e763aa322e34cedd5deb28d657980e82f9148
Kuma <98374456+MauroGomezz@users.noreply.github.com>
2022-09-30 21:18:14 -0300

    Update db.php

--- model/db.php
+++ model/db.php
@@ -5,7 +5,7 @@ class Conexion {
     private $con;
 
     public function __construct() {
-        $this->con = new mysqli('b4oawkqgde9flgj8wzlq-mysql.services.clever-cloud.com','uf2k2gn6ounhbz6o','YKxc0rJpigNMBtLHLFRQ','b4oawkqgde9flgj8wzlq');
+        $this->con = new mysqli('localhost','root','','biblioteca');
     }
 
     public function getLibros() {
